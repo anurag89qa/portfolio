840751275d2be693047d0ccafd2752c6b885914d
Anurag Mishra <amishra@opentable.com>
2022-01-29 17:32:53 +0530

    portfolio url change

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://anurag89qa.github.io/',
   title: 'JS.',
 }
 
